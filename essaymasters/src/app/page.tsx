4917855f2da838503760e31ae700d2d6c7daaa20
Benjamin Shonowo <bs455@kent.ac.uk>
2025-03-26 01:22:48 +0000

    Small changes

--- essaymasters/src/app/page.tsx
+++ essaymasters/src/app/page.tsx
@@ -1,41 +1,3 @@
-// "use client";
-
-// import { ArrowUpOnSquareIcon } from "@heroicons/react/24/outline";
-// import { SparklesIcon } from "@heroicons/react/16/solid";
-// import { ThemeProvider } from "next-themes";
-// import React from "react";
-// import Header from "./Components/Header";
-// import EssayForm from "./Components/EssayForm";
-// import TabsPanel from "./Components/TabsPanel";
-// import { useEffect, useState } from "react";
-// import { usePathname, useSearchParams } from "next/navigation";
-// import { useSession } from "next-auth/react";
-// import { useRouter } from "next/navigation";
-
-
-// export default function Home() {
-//   const { data: session, status } = useSession();
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     if (status === "unauthenticated") {
-//       router.push("/login"); // Redirect to login if not logged in
-//     }
-//   }, [status, router]);
-
-//   if (status === "loading") return <p>Loading...</p>; // Show loading while checking auth
-
-//   return (
-//     <div className="flex flex-col min-h-screen bg-white dark:bg-darker-custom">
-//       <Header />
-//       <main className="flex-1 flex justify-center p-6">
-//         <EssayForm />
-//       </main>
-//     </div>
-//   );
-// }
-
-
 "use client";
 
 import { useEffect, useState } from "react";
@@ -77,7 +39,7 @@ export default function Home() {
     }
   };
 
-  if (!sessionId) return <p>Loading session...</p>;
+  if (!sessionId) return <p className="color black center align text">Loading session...</p>;
 
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-darker-custom">
